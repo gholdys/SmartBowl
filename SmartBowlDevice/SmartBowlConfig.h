1dc3539d9bd544ce4b273ca4ab9b29938fc6ee5f
Grzegorz Hołdys <grzegorz.holdys@insightmachines.com>
2017-02-17 21:46:19 +0100

    Modified device config file

--- SmartBowlDevice/SmartBowlConfig.h
+++ SmartBowlDevice/SmartBowlConfig.h
@@ -1,4 +1,4 @@
 const char WiFiSSID[] = "Your SSID";
 const char WiFiPSK[] = "Your WiFi Password";
 const char SERVER[] = "Service URL";
-const String DEVICE_ID = "Some device ID";
+const String DEVICE_ID = "Some device id";
